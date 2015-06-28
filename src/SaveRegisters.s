6d44831dd7c6308d2b839aa66c7c0ea5c07d31db
mygamelife <mygamelife1@gmail.com>
2015-06-28 19:16:13 +0800

    Edit spacing in SaveRegisters.s

--- src/SaveRegisters.s
+++ src/SaveRegisters.s
@@ -29,65 +29,63 @@
 ;* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ; 
 ;*******************************************************************************
-								PRESERVE8
+		PRESERVE8
                 THUMB
 
                 AREA    MyCode, CODE, READONLY
-saveRegs		  	PROC
+saveRegs	PROC
                 EXPORT  saveRegs
                 IMPORT  initTcb
-								IMPORT  task1Tcb
-								
-								mov			r0, 	#0x0A					; used to verify
-								mov			r1, 	#0x0B					; used to verify
-								mov			r2, 	#0x0C					; used to verify
-								mov			r3, 	#0x0D					;	used to verify
-								mov			r4, 	#0x0E					;	used to verify
-								mov			r5, 	#0x0F					;	used to verify
-								mov			r6, 	#0xAA					;	used to verify
-								mov			r7, 	#0xBB					;	used to verify
-								mov			r8, 	#0xCC					;	used to verify
-								mov			r9, 	#0xDD					;	used to verify
-								mov			r10,	#0xEE					;	used to verify
-								mov			r11, 	#0xFF					;	used to verify
-								mov			r12, 	#0xAAA				;	used to verify
-								ldr			r5, =task1Tcb				; load task1Tcb address in R5
-								ldr			sp, [r5, #0x04]			;	load task1Tcb.sp[r1, #0x04] 0x20000418 into SP
-								
-								push	  {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12}		; Push all register to stack, except R13
-								
-								;		Decrement after
-								;		+---------+
-								;		|		r12		|	0x20000414
-								;		+---------+
-								;		|		r11		|	0x20000410
-								;		+---------+
-								;		|		r10		|	0x2000040C
-								;		+---------+
-								;		|		r9		|	0x20000408
-								;		+---------+
-								;		|		r8		|	0x20000404
-								;		+---------+
-								;		|		r7		|	0x20000400
-								;		+---------+
-								;		|		r6		|	0x200003FC
-								;		+---------+
-								;		|		r6		|	0x200003F8
-								;		+---------+
-								;		|		r5		|	0x200003F4
-								;		+---------+
-								;		|		r4		|	0x200003F0
-								;		+---------+
-								;		|		r3		|	0x200003EC
-								;		+---------+
-								;		|		r2		|	0x200003E8
-								;		+---------+
-								;		|		r1		|	0x200003E4
-								;		+---------+
-								;		|		r0		|	0x200003E0
-								;		+---------+
+		IMPORT  task1Tcb
+		
+		mov			r0, 	#0x0A		; used to verify
+		mov			r1, 	#0x0B		; used to verify
+		mov			r2, 	#0x0C		; used to verify
+		mov			r3, 	#0x0D		; used to verify
+		mov			r4, 	#0x0E		; used to verify
+		mov			r5, 	#0x0F		; used to verify
+		mov			r6, 	#0xAA		; used to verify
+		mov			r7, 	#0xBB		; used to verify
+		mov			r8, 	#0xCC		; used to verify
+		mov			r9, 	#0xDD		; used to verify
+		mov			r10,	#0xEE		; used to verify
+		mov			r11, 	#0xFF		; used to verify
+		mov			r12, 	#0xAAA		; used to verify
+		ldr			r5, =task1Tcb		; load task1Tcb address in R5
+		ldr			sp, [r5, #0x04]		; load task1Tcb.sp[r1, #0x04] 0x20000418 into SP
+		
+		push	  {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12}	; Push all register to stack, except R13
+		
+		;		Decrement after
+		;		+-------+
+		;		|  r12  |	0x20000414
+		;		+-------+
+		;		|  r11  |	0x20000410
+		;		+-------+
+		;		|  r10  |	0x2000040C
+		;		+-------+
+		;		|  r9   |	0x20000408
+		;		+-------+
+		;		|  r8   |	0x20000404
+		;		+-------+
+		;		|  r7   |	0x20000400
+		;		+-------+
+		;		|  r6   |	0x200003FC
+		;		+-------+
+		;		|  r5   |	0x200003F8
+		;		+-------+
+		;		|  r4   |	0x200003F4
+		;		+-------+
+		;		|  r3   |	0x200003F0
+		;		+-------+
+		;		|  r2   |	0x200003EC
+		;		+-------+
+		;		|  r1   |	0x200003E8
+		;		+-------+
+		;		|  r0   |	0x200003E4
+		;		+-------+
 
-								bx			lr									; Return to our caller
+		bx			lr	; Return to our caller
                 ENDP
-									
-								END
+		
+		END
