946e03245ff6ff81c75a6cb1302f24c1b6211d30
mygamelife <mygamelife1@gmail.com>
2015-06-28 19:19:54 +0800

    edit spacing in saveRegister.s

--- src/SaveRegisters.s
+++ src/SaveRegisters.s
@@ -29,32 +29,33 @@
 ;* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ; 
 ;*******************************************************************************
-		PRESERVE8
-                THUMB
-
-                AREA    MyCode, CODE, READONLY
+          PRESERVE8
+          THUMB
+          
+          AREA    MyCode, CODE, READONLY
 saveRegs	PROC
-                EXPORT  saveRegs
-                IMPORT  initTcb
-		IMPORT  task1Tcb
+          EXPORT  saveRegs
+          IMPORT  initTcb
+          IMPORT  task1Tcb
 		
-		mov			r0, 	#0x0A		; used to verify
-		mov			r1, 	#0x0B		; used to verify
-		mov			r2, 	#0x0C		; used to verify
-		mov			r3, 	#0x0D		; used to verify
-		mov			r4, 	#0x0E		; used to verify
-		mov			r5, 	#0x0F		; used to verify
-		mov			r6, 	#0xAA		; used to verify
-		mov			r7, 	#0xBB		; used to verify
-		mov			r8, 	#0xCC		; used to verify
-		mov			r9, 	#0xDD		; used to verify
-		mov			r10,	#0xEE		; used to verify
-		mov			r11, 	#0xFF		; used to verify
-		mov			r12, 	#0xAAA		; used to verify
-		ldr			r5, =task1Tcb		; load task1Tcb address in R5
-		ldr			sp, [r5, #0x04]		; load task1Tcb.sp[r1, #0x04] 0x20000418 into SP
+		mov  r0,  #0x0A		; used to verify
+		mov  r1,  #0x0B		; used to verify
+		mov  r2, 	#0x0C		; used to verify
+		mov  r3, 	#0x0D		; used to verify
+		mov  r4, 	#0x0E		; used to verify
+		mov  r5, 	#0x0F		; used to verify
+		mov  r6, 	#0xAA		; used to verify
+		mov  r7, 	#0xBB		; used to verify
+		mov  r8, 	#0xCC		; used to verify
+		mov  r9, 	#0xDD		; used to verify
+		mov  r10,	#0xEE		; used to verify
+		mov  r11, #0xFF		; used to verify
+		mov  r12, #0xAAA  ; used to verify
+    
+		ldr  r5, =task1Tcb    ; load task1Tcb address in R5
+		ldr  sp, [r5, #0x04]  ; load task1Tcb.sp[r1, #0x04] 0x20000418 into SP
 		
-		push	  {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12}	; Push all register to stack, except R13
+		push  {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12}	; Push all register to stack, except R13
 		
 		;		Decrement after
 		;		+-------+
@@ -84,8 +85,8 @@ saveRegs	PROC
 		;		+-------+
 		;		|  r0   |	0x200003E4
 		;		+-------+
-
-		bx			lr	; Return to our caller
-                ENDP
+    
+    bx			lr	; Return to our caller
+    ENDP
 		
 		END
